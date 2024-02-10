e70ac99239a96ad8a99e828aea1c252da2aaf469
Angela Davis <angela__davis@outlook.com>
2024-02-10 15:24:21 +0000

    Updated comments

--- Week02-statements/2Equals3check.py
+++ Week02-statements/2Equals3check.py
@@ -1,5 +1,5 @@
 #2Equals3check.py
-#This program checks is 2 is equal to 3 and returns a True or False verdict
+#This program checks if 2 is equal to 3 and returns a True or False verdict
 #Author: Angela Davis
 
 print (2==3)
