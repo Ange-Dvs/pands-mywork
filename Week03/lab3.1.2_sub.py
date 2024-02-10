eddfa601f36787d36d1581ea7fa544d2cd112890
Angela Davis <angela__davis@outlook.com>
2024-02-10 17:06:05 +0000

    Lab 3.1.2 completed + 3.1.2 updated

---
+++ Week03/lab3.1.2_sub.py
@@ -0,0 +1,8 @@
+#lab3.1.2_sub.py
+#This program takes in two numbers from a user and subtracts the first from the second one.
+#Author:Angela Davis
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+answer = x-y
+print ("{} minus {} is {} ".format (x, y, answer))
