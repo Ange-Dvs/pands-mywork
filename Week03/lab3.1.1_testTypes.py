5071b4f0fe4b4888ccab6c7c086b545d76e6ee96
Angela Davis <angela__davis@outlook.com>
2024-02-10 17:01:50 +0000

    lab 3.1.1 completed

---
+++ Week03/lab3.1.1_testTypes.py
@@ -0,0 +1,15 @@
+#lab3.1.1_testTypes.py
+#This program has 5 variables (int,float, boolean, str, list), using type() it checks the variables and prints which type each variable is. 
+#Author: Angela Davis
+
+i = 3
+fl = 3.5
+isa = True
+memo = 'how now Brown Cow'
+lots = []
+
+print('variable {} is of type: {}'.format('i', type(i), i))
+print('variable {} is of type: {}'.format('fl', type(fl), fl))
+print('variable {} is of type: {}'.format('isa', type(isa), isa))
+print('variable {} is of type: {}'.format('memo', type(memo), memo))
+print('variable {} is of type: {}'.format('lots', type(lots), lots))
