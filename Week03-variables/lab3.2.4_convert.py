0d3ac9ccac386805c4e17307df4625534da21e45
Angela Davis <angela__davis@outlook.com>
2024-02-21 18:05:25 +0000

    lab3.2.4_convert.py updated & completed

--- Week03-variables/lab3.2.4_convert.py
+++ Week03-variables/lab3.2.4_convert.py
@@ -2,17 +2,20 @@
 #This program takes in a float amount of dollars and returns that absolute amount in cents
 #Author: Angela Davis
 
-#Not finished
+amount = float(input('Please enter an amount:'))
 
-number= float(input("Please enter an amount:"))
-ab_number = abs(number)
+dollars, cents = map(int, str(amount).split('.'))
+# There are 4 main steps being carried out in the line above:
+ 
+#       1 & 2.    str(amount).split('.') - this converts the float amount taken in from the user to a string. The string is then split using '.' as the seperator.
+#                   You then have two strings the dollar amount as one string & cents amount as another.
+#       3.        map(int, ....) then takes the strings and converts them to integers
+#       4.        dollars, cents = - sets the variable names for the two integers derived from the string using the split function. 
 
-if number == 0.29:
-    amount_in_cents = int((ab_number*100)+1)
+absolute_cents = abs(dollars) * 100 + abs(cents) 
+# abs() function is used to convert the int values of the dollars and cents variables to their absolute number to avoid any negative numbers causing issues with the end calculation.
+# The two absolute values are added to give us the amount inputted by the user converted into cents.
 
-else:
-    amount_in_cents = int(ab_number*100)
+print (f'The amount in cent is: {absolute_cents}')
 
-print(f'The amount in cents is {amount_in_cents}')
-
-# maybe if function could be used? if 0.29 is entered amount_in_cents = int(ab_number*100)+1
+# It then calculates the absolute amount in cents by multiplying the absolute value in dollars by 100 and adding the absolute value of the cents. 
