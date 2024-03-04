caebcb4ec96e080d0587712b7616b651be8e9d42
Angela Davis <angela__davis@outlook.com>
2024-03-04 08:24:11 +0000

    Lab for week 6 completed

--- Week06-functions/lab6.1.2_students_explantion.py
+++ Week06-functions/lab6.1.2_students_explantion.py
@@ -1,11 +1,8 @@
 # lab6.1.2_students_expantion.py
 # 
-# create a program that allows a user to create new students and to view students.
-# Write a function that prints out a menu of commands we can perform, ie add, 
-# view and quit. The function should return what the user chose.
-# Test the function.
+# create a program that allows a user to create new students, add their modules and grades and to view students.
+# Author: Angela Davis
 
-# Not finished stopped at Q4 
 
 def show_menu():
     print ("What would you like to do?\n\t(a) Add new student\n\t(v) View students\n\t(q) Quit")
@@ -13,55 +10,50 @@ def show_menu():
     print (f"You chose {user_menu_choice}")
     return user_menu_choice
 
-#def repeat_menu ():
- #   if user_menu_choice != "q"
+def readModules():
+    modules = []
+    module_name = input("\tEnter the first module name (blank to quit):").strip()
 
-def doAdd():
+    while module_name != "":
+        module = {}
+        module["name"] = module_name
+        module["grade"] = int(input("\tEnter grade:"))
+        modules.append(module)
+        module_name = input("\tEnter next module name (blank to quit): ").strip()
+    return modules
+
+def doAdd(students):
     print ("You are in the adding section.")
-    student["name"] = input("Enter student's name: ")
-    
+    current_student = {}
+    current_student["name"] = input("Enter student's name: ")
+    current_student["modules"] = readModules () 
+    students.append(current_student)
 
-    
-def doView():
-    print ("You are in viewing.")
+def displayModules(modules):
+    print ("\tName  \tGrade")
+    for module in modules:
+        print(f"\t{module['name']}  \t{module['grade']}")
 
-student = {}
-    
+def doView(students):
+    print ("\nYou selected view.\n")
+    for currrent_student in students:
+        print (currrent_student["name"])
+        displayModules (currrent_student["modules"])
+
+students = []
+
 user_menu_choice = show_menu ()
 
 while user_menu_choice != "q":
     if user_menu_choice == "a":
-        doAdd()
+        doAdd(students)
         
     elif user_menu_choice == "v": 
-        doView()
+        doView(students)
 
     elif user_menu_choice != "q":
         print("\nPlease select either a,v or q")
 
     user_menu_choice = show_menu ()
 
-print (student)
-
-print("You have selected q. This program will now end")
-
-
-
-
-'''
-student = {}
-student["name"] = input("Enter student's name: ")
-
-modules = []
-while True:
-    course_name = input("Enter module course name: ")
-    if course_name == "":
-        break
-
-    module_grade = input("Enter the grade for the module: ")
-
-    modules.append({"course_name":course_name, "grade":module_grade})
-
-student["modules"] = modules
-
-print(student)'''
+print("This program will now end")
